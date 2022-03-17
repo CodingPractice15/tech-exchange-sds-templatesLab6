ae7290cd525705031cb71eec4f7901897d0b4fc7
Benjamin H <benjaminhansen836@gmail.com>
2022-03-17 13:47:29 -0600

    added notes

--- flask-template/app.py
+++ flask-template/app.py
@@ -18,6 +18,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from model import userStateAnswers
+# Team members Benjamin Hansen and Teqwon Norman
 
 # -- Initialization section --
 app = Flask(__name__)
